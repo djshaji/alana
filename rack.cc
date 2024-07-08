620f798d0ef2b3f8b27f8f0e37a79767b11b2609
Shaji Khan <quotablecode@gmail.com>
2024-07-08 20:57:13 +0530

    Mon Jul  8 08:57:13 PM IST 2024

--- rack.cc
+++ rack.cc
@@ -2,5 +2,5 @@
 
 void Rack::add () {
     engine -> addPlugin ("libs/dyson_compress_1403.so", 0, SharedLibrary::PluginType::LADSPA);
-    //~ LOGD ("plugin controls: %d", engine -> activePlugins->at (0)->pluginControls.size ());
+    LOGD ("plugin controls: %d", engine -> activePlugins->at (0)->pluginControls.size ());
 }
