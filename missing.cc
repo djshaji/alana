55f7e0afd6cea0e16ad1caffc09152da325fc05f
Shaji Khan <quotablecode@gmail.com>
2024-07-15 10:58:19 +0530

    linux ladspa plugins

--- missing.cc
+++ missing.cc
@@ -6,32 +6,23 @@
 #include "json.hpp"
 #include <lilv/lilv.h>
 
-int main (int argc, char ** argv ) {
-    std::string path = "libs";
-    std::vector <std::string> aplugins ;
+void list_ladspa (std::string path) {
     for (const auto & entry : std::filesystem::directory_iterator(path)) {
         std::cout << entry.path() << std::endl;
         
         SharedLibrary s = SharedLibrary ((char *)entry.path ().u8string ().c_str (), SharedLibrary::PluginType::LADSPA) ;
         if (s.load () == NULL) {
             for (int i = 0 ; i < s.descriptors.size () ; i ++) {
-                printf ("name: %s\n", s.descriptors.at (i)->Name);
-                aplugins.push_back (s.descriptors.at (i)->Name);
+                printf ("-----------------------------------\n");
+                printf ("[%s] %d: %s\n", entry.path().c_str (), i, s.descriptors.at (i)->Name);
+                printf ("-----------------------------------\n");
             }
-        }
-        
-        //~ for (int i = 0 ; i < plugins.size () ; i ++) {
-            //~ printf ("plugin: %s\n", aplugins.at (i).c_str ());
-        //~ }
-        
-        
+        }        
     }
-        
-    std::ifstream fJson(argv [1]);
-    std::stringstream buffer;
-    buffer << fJson.rdbuf();
-    auto json = nlohmann::json::parse(buffer.str());
+    
+}
 
+void list_lilv () {
     {
         LilvWorld* world = (LilvWorld* )lilv_world_new();
         lilv_world_load_all(world);
@@ -39,18 +30,29 @@ int main (int argc, char ** argv ) {
         LILV_FOREACH (plugins, i, plugins) {
             const LilvPlugin* p = (LilvPlugin* )lilv_plugins_get(plugins, i);
             const char * name = lilv_node_as_string (lilv_plugin_get_name (p));
-            printf("[LV2] %s\n", name);
-            aplugins.push_back (name);
-            
+            printf("[LV2] %s\n", name);            
         }
     }
+
+    
+}
+
+int main (int argc, char ** argv ) {
+    std::string path = "libs";
+        
+    std::ifstream fJson(argv [1]);
+    std::stringstream buffer;
+    buffer << fJson.rdbuf();
+    auto json = nlohmann::json::parse(buffer.str());
 
     std::cout << "\n\n-------| missing |----------" << "\n\n" ;
     
     for (auto plugin : json) {
-        if (! std::count (aplugins.begin (), aplugins.end (), plugin ["name"])) {
-            std::cout << plugin ["name"]<< "\n";
-        }
+        path = plugin ["library"].dump () ;
+        path = path.substr (1, path.size () - 2);
+        std::string fpath = std::string ("libs/") + path ;
+        if (! std::filesystem::exists(fpath))
+            printf ("[missing] %s\n", fpath.c_str ());
     }
     
     
