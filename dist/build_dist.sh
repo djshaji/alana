8d402acdb16d3fc34a0aafdb03391aefc6f0e53a
Shaji Khan <quotablecode@gmail.com>
2024-09-29 14:21:22 +0530

    dist

--- dist/build_dist.sh
+++ dist/build_dist.sh
@@ -1,3 +1,4 @@
 COUNT=`git rev-list HEAD --count`
 echo packing amprack $COUNT
+echo ../../releases/amprack-$COUNT.tar.gz
 tar czf ../../releases/amprack-$COUNT.tar.gz --dereference --hard-dereference amprack/
